eddacb5d1d7e74b4a3190fee70499a8a3c222019
nathanilo <nathanielodion@gmail.com>
2024-03-08 19:17:09 +0100

    Set up dummy products

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import products from "./products";
 
 const app = express();
 
@@ -8,6 +9,10 @@ app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Welcome to QwikShop API");
+});
+
+app.get("/products", (req, res) => {
+  res.send(products);
 });
 
 const PORT = process.env.PORT || 5000;
---
+++ backend/products.js
@@ -0,0 +1,28 @@
+const products = [
+  {
+    id: 1,
+    name: "iphone 12 pro",
+    price: 100,
+    description: "6.1inches, 4GB RAM, 64GB ROM, 12MP Camera, 3000mAh Battery",
+    image:
+      "https://res.cloudinary.com/dobhznnvd/image/upload/v1709921297/qwikshop/iphone12pro_bipbuc.jpg",
+  },
+  {
+    id: 2,
+    name: "iphone 12",
+    price: 200,
+    description: "5.4inches, 4GB RAM, 64GB ROM, 12MP Camera, 3000mAh Battery",
+    image:
+      "https://res.cloudinary.com/dobhznnvd/image/upload/v1709921297/qwikshop/iphone12_i1u3gc.jpg",
+  },
+  {
+    id: 3,
+    name: "Samsung Galaxy S21",
+    price: 200,
+    description: "5.4inches, 4GB RAM, 64GB ROM, 12MP Camera, 5000mAh Battery",
+    image:
+      "https://res.cloudinary.com/dobhznnvd/image/upload/v1709921296/qwikshop/galaxyS_vis68l.png",
+  },
+];
+
+export default products;
