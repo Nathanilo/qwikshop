506471624c440fd267c636dba5dc54fe64450874
nathanilo <nathanielodion@gmail.com>
2024-03-08 18:08:42 +0100

    Add error-404 page

--- frontend/src/components/PageNotFound.jsx
+++ frontend/src/components/PageNotFound.jsx
@@ -1,7 +1,21 @@
+import errorPageImage from "/images/404.jpg";
+import { Link } from "react-router-dom";
+
 const PageNotFound = () => {
   return (
     <>
-      <h1>Error 404: Page Not Found</h1>
+      <div className="error-page_container">
+        <div className="error-image_container">
+          <img
+            src={errorPageImage}
+            alt="Page Not Found"
+            className="error-image"
+          />
+        </div>
+        <Link to="/">
+          <button>Go back home</button>
+        </Link>
+      </div>
     </>
   );
 };
